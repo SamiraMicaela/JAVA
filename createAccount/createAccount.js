09d9f2773ca3fc323ca2099a4172217a074504d6
SamiraMicaela <micaelabava@hotmail.es>
2023-06-26 19:47:08 -0300

    Trabajo final

--- createAccount/createAccount.js
+++ createAccount/createAccount.js
@@ -1,4 +1,4 @@
-document.getElementById("form").addEventListener("submit", function(event){
+/*document.getElementById("form").addEventListener("submit", function(event){
     event.preventDefault();
     const name =document.getElementById("name").value;
     const email = document.getElementById("email").value;
@@ -10,9 +10,7 @@ document.getElementById("form").addEventListener("submit", function(event){
     console.log("Contraseña: "+ password);
 
     window.location.href = "../login/login.html?name=" + encodeURIComponent(name) + "&email=" + encodeURIComponent(email);
-});
-
-
+});*/
 /*este codigo escucha el evento SUBMIT del formulario y captura los valores de los campos NAME, EMAIL, PASSWORD.*/
 /*muestra los datos por  consola */
 /*La redireccion se realiza utilizando WINDOW.LOCATION.HREF*/
@@ -20,3 +18,17 @@ document.getElementById("form").addEventListener("submit", function(event){
 /*los pude pasar utilizando el formato "? nombreParametro=valorParametro"*/
 /*utilice ENCODEURICOMPONENT()  para asegurarme de que los valores se codifiquen +*/
 /*correctamente en caso de contener caracteres especiales */
+
+document.getElementById("form").addEventListener("submit", function (event){
+    event.preventDefault();
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    console.log("Datos ingresados: ");
+    console.log("Nombre: " + name);
+    console.log("Email: "+ email);
+    console.log("Contraseña: "+ password);
+
+})
